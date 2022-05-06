3ef1a080dd29d87b9f707c70853c7ef78c74d21f
Karol Leśniewski <lesniewski.karol99@gmail.com>
2022-05-06 18:34:18 +0200

    test zachowania

--- src/test/java/edu/iis/mto/testreactor/dishwasher/DishWasherTest.java
+++ src/test/java/edu/iis/mto/testreactor/dishwasher/DishWasherTest.java
@@ -1,6 +1,7 @@
 package edu.iis.mto.testreactor.dishwasher;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 
 import edu.iis.mto.testreactor.dishwasher.engine.Engine;
 import edu.iis.mto.testreactor.dishwasher.engine.EngineException;
@@ -9,7 +10,9 @@ import edu.iis.mto.testreactor.dishwasher.pump.WaterPump;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.mockito.InOrder;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 
@@ -130,6 +133,22 @@ class DishWasherTest {
         RunResult actualRunResult = dishWasher.start(programConfiguration);
         Assertions.assertEquals(expectedRunResult.getStatus(), actualRunResult.getStatus());
         Assertions.assertEquals(expectedRunResult.getRunMinutes(), actualRunResult.getRunMinutes());
+    }
+
+    @Test
+    void correctRunBehaviourTest() throws PumpException, EngineException {
+        Mockito.when(door.closed()).thenReturn(true);
+        Mockito.when(dirtFilter.capacity()).thenReturn(60.0);
+        ProgramConfiguration programConfiguration = generateProgramConfiguration(WashingProgram.ECO, FillLevel.HALF, true);
+        dishWasher.start(programConfiguration);
+        InOrder inOrder = Mockito.inOrder(door, dirtFilter, waterPump, engine);
+        inOrder.verify(door).closed();
+        inOrder.verify(dirtFilter).capacity();
+        inOrder.verify(door).lock();
+        inOrder.verify(waterPump).pour(any(FillLevel.class));
+        inOrder.verify(engine).runProgram(any());
+        inOrder.verify(waterPump).drain();
+        inOrder.verify(door).unlock();
     }
 
 
