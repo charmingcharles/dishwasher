6dd15ee9536a3e955a541804d4fc5dcdbb889c37
Karol Leśniewski <lesniewski.karol99@gmail.com>
2022-05-06 17:54:27 +0200

    pierwszy działający w pełni test

--- src/test/java/edu/iis/mto/testreactor/dishwasher/DishWasherTest.java
+++ src/test/java/edu/iis/mto/testreactor/dishwasher/DishWasherTest.java
@@ -5,10 +5,12 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import edu.iis.mto.testreactor.dishwasher.engine.Engine;
 import edu.iis.mto.testreactor.dishwasher.pump.WaterPump;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+
 
 class DishWasherTest {
 
@@ -26,6 +28,21 @@ class DishWasherTest {
 
     DishWasher dishWasher;
 
+    ProgramConfiguration generateProgramConfiguration(WashingProgram washingProgram, FillLevel fillLevel, boolean tabletsUsed){
+        return ProgramConfiguration.builder()
+                .withProgram(washingProgram)
+                .withFillLevel(fillLevel)
+                .withTabletsUsed(tabletsUsed)
+                .build();
+    }
+
+    RunResult generateRunResult(Status status, int runMinutes){
+        return RunResult.builder()
+                .withStatus(Status.SUCCESS)
+                .withRunMinutes(runMinutes)
+                .build();
+    }
+
     @BeforeEach
     void setUp(){
         waterPump = Mockito.mock(WaterPump.class);
@@ -38,6 +55,17 @@ class DishWasherTest {
     @Test
     void itCompiles() {
         assertThat(true, Matchers.equalTo(true));
+    }
+
+    @Test
+    void dishWasherCorrectRunTest(){
+        Mockito.when(door.closed()).thenReturn(true);
+        Mockito.when(dirtFilter.capacity()).thenReturn(60.0);
+        ProgramConfiguration programConfiguration = generateProgramConfiguration(WashingProgram.ECO, FillLevel.HALF, true);
+        RunResult expectedRunResult = generateRunResult(Status.SUCCESS, programConfiguration.getProgram().getTimeInMinutes());
+        RunResult actualRunResult = dishWasher.start(programConfiguration);
+        Assertions.assertEquals(expectedRunResult.getStatus(), actualRunResult.getStatus());
+        Assertions.assertEquals(expectedRunResult.getRunMinutes(), actualRunResult.getRunMinutes());
     }
 
 
